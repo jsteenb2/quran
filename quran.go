8ddc1480349f9895e95e3b08af6955f91b75b417
jsteenb2 <jonathansteenbergen@gmail.com>
2017-09-04 21:07:30 -0500

    add handler for all quran translations and save them to boltDB instance

---
+++ quran.go
@@ -0,0 +1,44 @@
+package quran
+
+import (
+	"log"
+
+	"github.com/boltdb/bolt"
+	"github.com/jsteenb2/quran/api"
+	"github.com/jsteenb2/quran/model"
+	"github.com/jsteenb2/quran/parser"
+)
+
+var (
+	quranBucket = []byte("quran")
+)
+
+
+func BuildQuranDB(db *bolt.DB) error {
+	editions, err := api.GetTextTranslationEditions()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	var edition model.QuranMeta
+	for idx := range editions.Editions {
+		edition = BuildQuran(editions.Editions[idx].Identifier)
+		check(edition.Save(db, quranBucket))
+	}
+	return nil
+}
+
+func BuildQuran(edition string) model.QuranMeta {
+	apiQuran, err := api.GetQuranContent(edition)
+	check(err)
+	parsedQuran := parser.ParseQuran()
+
+	return model.NewQuranMeta(parsedQuran, apiQuran)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
---
+++ quran_test.go
@@ -0,0 +1,93 @@
+package quran_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/boltdb/bolt"
+	"github.com/jsteenb2/quran"
+	"github.com/jsteenb2/quran/model"
+)
+
+func TestBuildQuran(t *testing.T) {
+	quranComplete := quran.BuildQuran("en.sahih")
+
+	if len(quranComplete.Suwar) != 114 {
+		t.Errorf("Wrong number of suwar, expected `%d`, got `%d`", 114, len(quranComplete.Suwar))
+	}
+
+	arRahman := quranComplete.Suwar[54]
+	if len(arRahman.Ayaat) != 78 {
+		t.Errorf("Wrong number of ayaat for %s, expected `%d` got `%d`", arRahman.EnglishName, 78, len(arRahman.Ayaat))
+	}
+
+	if arRahman.Ayaat[0].Text != "ٱلرَّحْمَٰنُ" {
+		t.Errorf("Wrong text received, expected `%s`, got `%s`", "ٱلرَّحْمَٰنُ", arRahman.Ayaat[0].Text)
+	}
+
+	if arRahman.Ayaat[3].Translation != "[And] taught him eloquence." {
+		t.Errorf("Wrong text received, expected `%s`, got `%s`", "[And] taught him eloquence.", arRahman.Ayaat[3].Translation)
+	}
+}
+
+func TestBuildQuran_Indonesian(t *testing.T) {
+	quranComplete := quran.BuildQuran("id.muntakhab")
+
+	if len(quranComplete.Suwar) != 114 {
+		t.Errorf("Wrong number of suwar, expected `%d`, got `%d`", 114, len(quranComplete.Suwar))
+	}
+
+	arRahman := quranComplete.Suwar[54]
+	if len(arRahman.Ayaat) != 78 {
+		t.Errorf("Wrong number of ayaat for %s, expected `%d` got `%d`", arRahman.EnglishName, 78, len(arRahman.Ayaat))
+	}
+
+	if arRahman.Ayaat[0].Text != "ٱلرَّحْمَٰنُ" {
+		t.Errorf("Wrong text received, expected `%s`, got `%s`", "ٱلرَّحْمَٰنُ", arRahman.Ayaat[0].Text)
+	}
+
+	expectedTranslation := "Dia menciptakan dan mengajarkan manusia kemampuan menjelaskan apa yang ada dalam dirinya, untuk membedakan dirinya dari makhluk lain."
+	if arRahman.Ayaat[3].Translation != expectedTranslation {
+		t.Errorf("Wrong text received, expected `%s`, got `%s`", expectedTranslation, arRahman.Ayaat[3].Translation)
+	}
+}
+
+func TestBuildQuranDB(t *testing.T) {
+	path := fmt.Sprintf("%s/src/github.com/jsteenb2/quran", os.Getenv("GOPATH"))
+	db, err := bolt.Open(path+"/quran.db", 0644, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = quran.BuildQuranDB(db)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sahih, err := model.GetQuran([]byte("quran"), []byte("en.sahih"), db)
+
+	if sahih.Identifier != "en.sahih" {
+		t.Errorf("unexpected quran edition, expected `en.sahih`, got `%s`", sahih.Identifier)
+	}
+
+	expectedTitle := "Al-Faatiha"
+	if sahih.Suwar[0].EnglishName != expectedTitle {
+		t.Errorf("unexpected Sura returned, expected `%s`, got `%s`", expectedTitle, sahih.Suwar[0].EnglishName)
+	}
+
+	expectedAyahText := "By time,"
+	if translation := sahih.Suwar[102].Ayaat[0].Translation; translation != expectedAyahText {
+		t.Errorf("incorrect ayah received, expected `%s`, got `%s`", expectedAyahText, translation)
+	}
+
+	indo, err := model.GetQuran([]byte("quran"), []byte("id.muntakhab"), db)
+	if indo.Identifier != "id.muntakhab" {
+		t.Errorf("unexpected quran edition, expected `id.muntakhab`, got `%s`", indo.Identifier)
+	}
+
+	if suwarsLen := len(indo.Suwar); suwarsLen != 114 {
+		t.Errorf("unexpected # suwar, expected `%d`, got `%d`", 114, suwarsLen)
+	}
+}
